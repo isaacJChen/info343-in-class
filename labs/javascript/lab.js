fc1e3ce441845d576188b276e42fe9010c9354a6
isaacJChen <isaacjc@uw.edu>
2017-10-13 11:42:25 -0700

    labs

--- labs/javascript/lab.js
+++ labs/javascript/lab.js
@@ -1,2 +1,7 @@
 // Put your JavaScript lab code here!
+let s = 'I like JavaScript more than I like to party.';
+console.log(s.substring(28, 43));
 
+let s2 = 'I love Washington State Unviersity';
+s2.replace('Washington State Unviersity', 'University of Washington');
+console.log(s2);
